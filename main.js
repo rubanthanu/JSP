d8812f27db01dd81ecb2b1265b01acc102bf8a66
rubanthanu <rubanthanu66@gmail.com>
2026-03-27 10:30:33 +0530

    class

--- main.js
+++ main.js
@@ -1,10 +1,18 @@
-const myobj={
-    name:"John",
-    age:30,
-    city:"New York"};
-console.log(Object.keys(myobj)  );
-
-for (let x in myobj) {
-    console.log(x);
+class pizaa{
+    constructor(name, price, size){
+        this.name = name;
+        this.price = price;
+        this.size = size;
+    }
+    print() {
+        console.log(`Name: ${this.name}, Price: $${this.price}, Size: ${this.size}`);
+    }
 }
-console.log(myobj.hasOwnProperty("name")  );
+
+
+let p1= new pizaa("Margherita", 8, "Medium");
+let p2= new pizaa("Pepperoni", 10, "Large");
+let p3= new pizaa("Veggie", 9, "Small");    
+p1.print();
+p2.print();
+p3.print();
