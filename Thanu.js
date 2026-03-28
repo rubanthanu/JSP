30dfec0ac842eb320b580fcd075e760b9d37466c
rubanthanu <rubanthanu66@gmail.com>
2026-03-28 19:16:14 +0530

    Finished

---
+++ Thanu.js
@@ -0,0 +1,12 @@
+
+const earnMoney = () => {
+return "Earn Monex by learning new skills and working hard!";
+};
+const growMoney = () => {
+return "Grow Money by investing in yourself!";
+};
+const giveMoney = () => {
+return "Give Money for Education and Charity!";
+}
+export default earnMoney;
+export { growMoney, giveMoney};
--- main.js
+++ main.js
@@ -1,24 +1,2 @@
-document.addEventListener("readystatechange", (event) => {
-    if(event.target.readyState === "complete") {
-        console.log("DOM fully loaded and parsed");
-        initApp();
-
-}});
-
-function initApp() {
-    console.log("Initializing application...");
-    // Add your initialization code here
-    const myContent = ["Earn", "Grow", "Give"];
-const myObject = {
-name: "Dhuddu",
-myContent: ["Earn", "Grow", "Give"],
-logName: function() {
-console.log(this.name);
-}
-};
-
-window.localStorage.setItem("myLocalStore", JSON.stringify(myObject));
-
-const myLocalData = JSON.parse(localStorage.getItem("myLocalStore"));
-console.log(myLocalData);
-}
+import earnMoney from "./Thanu.js";
+console.log(earnMoney());
